64a18040e3473f47efb2d56caf595c4e25464ecc
kalyan <kalyan.2405@gmail.com>
2022-08-18 17:10:56 +0530

    pagetest

--- core/views.py
+++ core/views.py
@@ -58,7 +58,7 @@ def update(request,uid):
             }
             return render(request, 'dashboard.html',context)   
         else:
-            p=Profile.objects.filter(user_id=uid)
+            p=Profile.objects.get(user_id=uid)
             message=f'{p.qrcode}-{p.token}'
             context={
             'message':message,
