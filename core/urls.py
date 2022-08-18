865d1d79f208728583f88a4ab66f57a2ad32f7e1
kalyan <kalyan.2405@gmail.com>
2022-08-18 17:24:44 +0530

    pagetest

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from core import views as core_views
+from django.conf import settings  
+from django.conf.urls.static import static  
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    
+    
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
